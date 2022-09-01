686bc794588792fd28288ea591cca2f6d7c2d4cd
BigDogStatusMoves <111360939+BigDogStatusMoves@users.noreply.github.com>
2022-09-01 08:05:13 -0500

    gaming

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val lemonadeStandSupplies = arrayOf("Wooden Stand","Lemonade","Cups","Money Jar","Sign")
+    for (element in lemonadeStandSupplies)
+        println(element)
+
+
+
+}
